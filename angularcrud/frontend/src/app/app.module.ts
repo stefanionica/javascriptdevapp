7c174a3a81fe8746dfbe1c4641eac0eb3f7b574a
Stefan Ionica <stefan.ionica@histria.lan>
2022-06-14 21:02:51 +0300

    javascriptdevapp

--- angularcrud/frontend/src/app/app.module.ts
+++ angularcrud/frontend/src/app/app.module.ts
@@ -1,23 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms'
+import { FormsModule} from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AlertaService } from './alerta.service';
+import { ApiService } from './api.service';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
-    AlertaService
+    ApiService
   ],
   bootstrap: [AppComponent]
 })
--- curs1/angulardemo/src/app/home/home.component.ts
+++ curs1/angulardemo/src/app/home/home.component.ts
@@ -1,6 +1,15 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { AlertaService } from '../alerta.service';
 import { User } from '../models/user';
+const HEROES = [
+  { id: 1, name: 'Superman' },
+  { id: 2, name: 'Batman' },
+  { id: 5, name: 'BatGirl' },
+  { id: 3, name: 'Robin' },
+  { id: 4, name: 'Flash' }
 
+];
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,6 +21,11 @@ import { User } from '../models/user';
 export class HomeComponent implements OnInit {
 
 title:string = 'Componenta Home !!';
+salut: string='Bine ati venit la cursul 2';
+nume: string='LinkAcademy';
+
+eroi= HEROES;
+
 user:User = {
   id:1,
   nume:'Popescu',
@@ -21,9 +35,10 @@ user:User = {
 image:string = 'https://i.pinimg.com/474x/92/cd/54/92cd54945a34b1a7a75c22901add2643.jpg'
 showImage: boolean = true;  
 
-constructor() { }
+constructor( private alertaService: AlertaService) { }
 
   ngOnInit(): void {
+    this.alertaService.showAlert('Salut Mihai');
   }
 afiseazaImagine(){
   this.showImage = !this.showImage;
